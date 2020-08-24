27e0ae05b27873ae3fa63a5c4f0430bc921e5121
jasonIV <thuhtetaung.d2@gmail.com>
2020-08-24 19:15:04 +0630

    config

---
+++ config.js
@@ -0,0 +1,2 @@
+export const baseApiUrl = "https://efi2torz90.execute-api.ap-southeast-1.amazonaws.com/production";
+export const mtkUrl="https://gja2c19uy2.execute-api.ap-southeast-1.amazonaws.com/prod/api/relay"
